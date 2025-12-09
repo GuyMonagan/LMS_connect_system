44c4a63b2fb1b2a1c815d5a660e97591fd3e87f9
GuyMonagan <werevalkury@gmail.com>
2025-12-09 14:12:18 +0400

    чистка: удалён celery-beat, таски перенесены, оптимизирована отправка email

--- users/tasks.py
+++ users/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from .models import Course, Subscription
+from materials.models import Course, Subscription
 from celery import shared_task
 from django.utils import timezone
 from datetime import timedelta
@@ -10,16 +10,15 @@ from users.models import User
 @shared_task
 def send_course_update_email(course_id):
     course = Course.objects.get(id=course_id)
-    subscribers = Subscription.objects.filter(course=course).select_related('user')
+    subscriber_emails = Subscription.objects.filter(course=course).values_list('user__email', flat=True)
 
-    for sub in subscribers:
-        user = sub.user
+    for email in subscriber_emails:
         send_mail(
             subject=f'Обновление курса: {course.title}',
-            message=f'Здравствуйте, {user.email}!\n\nКурс "{course.title}" был обновлён. Проверьте новые материалы на платформе.',
+            message=f'Здравствуйте, {email}!\n\nКурс "{course.title}" был обновлён. Проверьте новые материалы на платформе.',
             from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-            fail_silently=False,  # Теперь ошибки будут отображаться
+            recipient_list=[email],
+            fail_silently=False,
         )
 
 
